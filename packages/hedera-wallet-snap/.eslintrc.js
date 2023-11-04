f1e673c3b0aebb44e47b8bff0ad97f4a8274de5f
Kiran Pachhai <kiran@pachhai.com>
2023-11-04 07:45:29 -0400

    Fixed some issues that were occuring with incompatible yarn version a… (#9)
    Signed-off-by: Kipkap <kipkap1001@gmail.com>
    Co-authored-by: Kipkap <kipkap1001@gmail.com>

---
+++ packages/hedera-wallet-snap/.eslintrc.js
@@ -0,0 +1,49 @@
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+
+  extends: ['@metamask/eslint-config'],
+
+  overrides: [
+    {
+      files: ['**/*.js'],
+      extends: ['@metamask/eslint-config-nodejs'],
+    },
+
+    {
+      files: ['**/*.{ts,tsx}'],
+      extends: ['@metamask/eslint-config-typescript'],
+      rules: {
+        '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+        '@typescript-eslint/promise-function-async': 'off',
+        '@typescript-eslint/naming-convention': 'off',
+        'import/order': 'off',
+      },
+    },
+
+    {
+      files: ['**/*.test.ts', '**/*.test.js'],
+      extends: ['@metamask/eslint-config-jest'],
+      rules: {
+        '@typescript-eslint/no-shadow': [
+          'error',
+          { allow: ['describe', 'expect', 'it'] },
+        ],
+      },
+    },
+  ],
+
+  ignorePatterns: [
+    '!.prettierrc.js',
+    '**/!.eslintrc.js',
+    '**/dist*/',
+    '**/backend',
+    '**/*__GENERATED__*',
+    '**/build',
+    '**/public',
+    '**/.cache',
+  ],
+};
