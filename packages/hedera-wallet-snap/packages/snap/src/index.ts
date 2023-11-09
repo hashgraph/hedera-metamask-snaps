cf5a84ab41b59b4853c5d665bafac155cc31c102
Kipkap <kipkap1001@gmail.com>
2023-11-09 11:53:13 -0500

    Updated the text for hello API
    Signed-off-by: Kipkap <kipkap1001@gmail.com>

--- packages/hedera-wallet-snap/packages/snap/src/index.ts
+++ packages/hedera-wallet-snap/packages/snap/src/index.ts
@@ -96,9 +96,6 @@ export const onRpcRequest: OnRpcRequestHandler = async ({
           content: panel([
             text(`Hello, **${origin}**!`),
             text('This custom confirmation is just for display purposes.'),
-            text(
-              'But you can edit the snap source code to make it do something, if you want to!',
-            ),
           ]),
         },
       });
