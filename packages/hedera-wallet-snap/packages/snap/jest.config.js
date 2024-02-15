590f8d75d68d3b13aa99487b7985634b152599f2
Ivy Astrix <ivy.astrix@gmail.com>
2024-02-15 07:22:14 -0800

    statee static class references updated, created unit test + updated jest config
    Signed-off-by: Ivy Astrix <ivy.astrix@gmail.com>

--- packages/hedera-wallet-snap/packages/snap/jest.config.js
+++ packages/hedera-wallet-snap/packages/snap/jest.config.js
@@ -2,25 +2,8 @@
  * For a detailed explanation regarding each configuration property and type check, visit:
  * https://jestjs.io/docs/configuration
  */
-
-const config = {
-  // Automatically clear mock calls, instances, contexts and results before every test
+module.exports = {
   clearMocks: false,
-  // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: false,
-  // The directory where Jest should output its coverage files
-  coverageDirectory: 'coverage',
-  // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ['/node_modules/', 'tests'],
-  // Indicates which provider should be used to instrument code for coverage
-  coverageProvider: 'v8',
-  // An array of file extensions your modules use
-  moduleFileExtensions: ['js', 'json', 'ts', 'mjs', 'cjs'],
-  // The test environment that will be used for testing
-  testEnvironment: 'node',
-  // The regexp pattern or array of patterns that Jest uses to detect test files
-  testRegex: '.*\\.spec\\.ts$',
-  // A set of global variables that need to be available in all test environments
   globals: {
     window: {
       location: {
@@ -28,9 +11,10 @@ const config = {
       },
     },
   },
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
+  moduleFileExtensions: ['js', 'json', 'ts', 'mjs', 'cjs'],
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  testRegex: '.*\\.spec\\.ts$',
   transformIgnorePatterns: [],
-  testTimeout: 120000,
+  testTimeout: 120000
 };
-
-export default config;
