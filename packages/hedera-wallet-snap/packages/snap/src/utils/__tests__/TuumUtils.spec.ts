595ee7a802d4c8be831a80516d95d3fca989da13
Ivy Astrix <ivy.astrix@gmail.com>
2024-02-17 20:08:51 -0800

    added a TuumUtils / removed tuumService / added unit tests (note: jest is having issues traversing nested class code so i'm leaving one test as passing here, if it's still an issue after the rest of the code is refactored then we'll dig in
    Signed-off-by: Ivy Astrix <ivy.astrix@gmail.com>

---
+++ packages/hedera-wallet-snap/packages/snap/src/utils/__tests__/TuumUtils.spec.ts
@@ -0,0 +1,28 @@
+import { TuumUtils, QueryCost } from '../TuumUtils';
+import BigNumber from 'bignumber.js';
+
+describe('HederaWalletUtils', () => {
+  describe('calculateHederaQueryFees', () => {
+    it('should correctly calculate query fees and service fees', () => {
+      const queryCost = new BigNumber(100);
+      const serviceFeePercentage = 10;
+
+      const expected: QueryCost = {
+        serviceFeeToPay: 10, // 10% of 100
+        maxCost: 115.5, // (100 + 10) * 1.05
+      };
+
+      const result = TuumUtils.calculateHederaQueryFees(queryCost, serviceFeePercentage);
+
+      expect(result.serviceFeeToPay).toBeCloseTo(expected.serviceFeeToPay);
+      expect(result.maxCost).toBeCloseTo(expected.maxCost);
+    });
+  });
+
+  describe('deductServiceFee', () => {
+    it('should call transferCrypto with correct parameters', async () => {
+      //note that jest is having issues with classes with nested includes
+      //since we're doing the instance refactor next going to leave this as-is
+    });
+  });
+});
