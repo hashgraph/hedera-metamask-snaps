1ee4b51f2186bc8789a51e6d025eaae9238e5354
Ivy Astrix <ivy.astrix@gmail.com>
2024-02-28 15:41:12 -0800

    going with the command pattern to decouple them from modelling a logical client object: approve allowance, stake hbar and transfer crypto done
    Signed-off-by: Ivy Astrix <ivy.astrix@gmail.com>

--- packages/hedera-wallet-snap/packages/snap/src/client/SimpleHederaClientImpl.ts
+++ packages/hedera-wallet-snap/packages/snap/src/client/SimpleHederaClientImpl.ts
@@ -41,15 +41,8 @@ import {
 } from '@hashgraph/sdk';
 
 import { AccountInfo } from '../types/account';
-import {
-  SimpleHederaClient,
-  SimpleTransfer,
-  TxReceipt,
-} from '../types/hedera';
-import {
-  ApproveAllowanceAssetDetail,
-  TokenCustomFee,
-} from '../types/params';
+import { SimpleHederaClient, SimpleTransfer, TxReceipt } from '../types/hedera';
+import { ApproveAllowanceAssetDetail, TokenCustomFee } from '../types/params';
 import { Utils } from '../utils/Utils';
 import { CryptoUtils } from '../utils/CryptoUtils';
 import {
@@ -101,31 +94,11 @@ export class SimpleHederaClientImpl implements SimpleHederaClient {
     return this.#getAccountBalance(this._client);
   }
 
-  async transferCrypto(options: {
-    transfers: SimpleTransfer[];
-    memo: string | null;
-    maxFee: number | null;
-    serviceFeesToPay: Record<string, number>;
-    serviceFeeToAddress: string | null;
-    onBeforeConfirm?: () => void;
-  }): Promise<TxReceipt> {
-    return this.#transferCrypto(this._client, options);
-  }
-
   async stakeHbar(options: {
     nodeId: number | null;
     accountId: string | null;
   }): Promise<TxReceipt> {
     return this.#stakeHbar(this._client, options);
-  }
-
-  async approveAllowance(options: {
-    spenderAccountId: string;
-    amount: number;
-    assetType: string;
-    assetDetail?: ApproveAllowanceAssetDetail;
-  }): Promise<TxReceipt> {
-    return this.#approveAllowance(this._client, options);
   }
 
   async deleteAllowance(options: {
@@ -166,7 +139,11 @@ export class SimpleHederaClientImpl implements SimpleHederaClient {
     feeSchedulePublicKey: string | undefined;
     customFees: TokenCustomFee[] | undefined;
   }): Promise<TxReceipt> {
-    return this.#createToken(this._client, this._privateKey as PrivateKey, options);
+    return this.#createToken(
+      this._client,
+      this._privateKey as PrivateKey,
+      options,
+    );
   }
 
   async #getAccountBalance(client: Client): Promise<number> {
@@ -180,89 +157,6 @@ export class SimpleHederaClientImpl implements SimpleHederaClient {
 
     const amount = accountBalance.hbars.to(HbarUnit.Hbar);
     return amount.toNumber();
-  }
-
-  async #approveAllowance(
-    client: Client,
-    options: {
-      spenderAccountId: string;
-      amount: number;
-      assetType: string;
-      assetDetail?: ApproveAllowanceAssetDetail;
-    },
-  ): Promise<TxReceipt> {
-    const transaction = new AccountAllowanceApproveTransaction();
-
-    if (options.assetType === 'HBAR') {
-      transaction.approveHbarAllowance(
-        client.operatorAccountId as AccountId,
-        options.spenderAccountId,
-        new Hbar(options.amount),
-      );
-    } else if (options.assetType === 'TOKEN') {
-      const multiplier = Math.pow(
-        10,
-        options.assetDetail?.assetDecimals as number,
-      );
-      transaction.approveTokenAllowance(
-        options.assetDetail?.assetId as string,
-        client.operatorAccountId as AccountId,
-        options.spenderAccountId,
-        options.amount * multiplier,
-      );
-    } else if (options.assetType === 'NFT') {
-      if (options.assetDetail?.all) {
-        transaction.approveTokenNftAllowanceAllSerials(
-          options.assetDetail?.assetId,
-          client.operatorAccountId as AccountId,
-          options.spenderAccountId,
-        );
-      } else {
-        transaction.approveTokenNftAllowance(
-          options.assetDetail?.assetId as string,
-          client.operatorAccountId as AccountId,
-          options.spenderAccountId,
-        );
-      }
-    }
-
-    transaction.freezeWith(client);
-
-    const txResponse = await transaction.execute(client);
-
-    const receipt = await txResponse.getReceipt(client);
-
-    let newExchangeRate;
-    if (receipt.exchangeRate) {
-      newExchangeRate = {
-        ...receipt.exchangeRate,
-        expirationTime: Utils.timestampToString(
-          receipt.exchangeRate.expirationTime,
-        ),
-      };
-    }
-
-    return {
-      status: receipt.status.toString(),
-      accountId: receipt.accountId ? receipt.accountId.toString() : '',
-      fileId: receipt.fileId ? receipt.fileId : '',
-      contractId: receipt.contractId ? receipt.contractId : '',
-      topicId: receipt.topicId ? receipt.topicId : '',
-      tokenId: receipt.tokenId ? receipt.tokenId : '',
-      scheduleId: receipt.scheduleId ? receipt.scheduleId : '',
-      exchangeRate: newExchangeRate,
-      topicSequenceNumber: receipt.topicSequenceNumber
-        ? String(receipt.topicSequenceNumber)
-        : '',
-      topicRunningHash: CryptoUtils.uint8ArrayToHex(receipt.topicRunningHash),
-      totalSupply: receipt.totalSupply ? String(receipt.totalSupply) : '',
-      scheduledTransactionId: receipt.scheduledTransactionId
-        ? receipt.scheduledTransactionId.toString()
-        : '',
-      serials: JSON.parse(JSON.stringify(receipt.serials)),
-      duplicates: JSON.parse(JSON.stringify(receipt.duplicates)),
-      children: JSON.parse(JSON.stringify(receipt.children)),
-    } as TxReceipt;
   }
 
   async #deleteAccount(
@@ -480,152 +374,6 @@ export class SimpleHederaClientImpl implements SimpleHederaClient {
     transaction.freezeWith(client);
 
     const txResponse = await transaction.execute(client);
-
-    const receipt = await txResponse.getReceipt(client);
-
-    let newExchangeRate;
-    if (receipt.exchangeRate) {
-      newExchangeRate = {
-        ...receipt.exchangeRate,
-        expirationTime: Utils.timestampToString(
-          receipt.exchangeRate.expirationTime,
-        ),
-      };
-    }
-
-    return {
-      status: receipt.status.toString(),
-      accountId: receipt.accountId ? receipt.accountId.toString() : '',
-      fileId: receipt.fileId ? receipt.fileId : '',
-      contractId: receipt.contractId ? receipt.contractId : '',
-      topicId: receipt.topicId ? receipt.topicId : '',
-      tokenId: receipt.tokenId ? receipt.tokenId : '',
-      scheduleId: receipt.scheduleId ? receipt.scheduleId : '',
-      exchangeRate: newExchangeRate,
-      topicSequenceNumber: receipt.topicSequenceNumber
-        ? String(receipt.topicSequenceNumber)
-        : '',
-      topicRunningHash: CryptoUtils.uint8ArrayToHex(receipt.topicRunningHash),
-      totalSupply: receipt.totalSupply ? String(receipt.totalSupply) : '',
-      scheduledTransactionId: receipt.scheduledTransactionId
-        ? receipt.scheduledTransactionId.toString()
-        : '',
-      serials: JSON.parse(JSON.stringify(receipt.serials)),
-      duplicates: JSON.parse(JSON.stringify(receipt.duplicates)),
-      children: JSON.parse(JSON.stringify(receipt.children)),
-    } as TxReceipt;
-  }
-
-  async #transferCrypto(
-    client: Client,
-    options: {
-      transfers: SimpleTransfer[];
-      memo: string | null;
-      maxFee: number | null; // hbar
-      serviceFeesToPay: Record<string, number>;
-      serviceFeeToAddress: string | null;
-      onBeforeConfirm?: () => void;
-    },
-  ): Promise<TxReceipt> {
-    const serviceFeeToAddr: string = options.serviceFeeToAddress ?? '0.0.98'; // 0.0.98 is Hedera Fee collection account
-
-    const transaction = new TransferTransaction().setTransactionMemo(
-      options.memo ?? '',
-    );
-
-    if (options.maxFee) {
-      transaction.setMaxTransactionFee(new Hbar(options.maxFee.toFixed(8)));
-    }
-
-    for (const transfer of options.transfers) {
-      if (transfer.assetType === 'HBAR') {
-        if (ethers.isAddress(transfer.to)) {
-          transfer.to = `0.0.${transfer.to.slice(2)}`;
-        }
-
-        transaction.addHbarTransfer(transfer.to, transfer.amount);
-        if (_.isEmpty(transfer.from)) {
-          transaction.addHbarTransfer(
-            client.operatorAccountId as AccountId,
-            -transfer.amount,
-          );
-        } else {
-          transaction.addApprovedHbarTransfer(
-            transfer.from as string,
-            -transfer.amount,
-          );
-        }
-
-        // Service Fee
-        if (options.serviceFeesToPay[transfer.assetType] > 0) {
-          transaction.addHbarTransfer(
-            serviceFeeToAddr,
-            options.serviceFeesToPay[transfer.assetType],
-          );
-          transaction.addHbarTransfer(
-            client.operatorAccountId as AccountId,
-            -options.serviceFeesToPay[transfer.assetType],
-          );
-        }
-      } else if (transfer.assetType === 'TOKEN') {
-        const assetid = transfer.assetId as string;
-        const multiplier = Math.pow(10, transfer.decimals as number);
-
-        transaction.addTokenTransfer(
-          assetid,
-          transfer.to,
-          transfer.amount * multiplier,
-        );
-        if (_.isEmpty(transfer.from)) {
-          transaction.addTokenTransfer(
-            assetid,
-            client.operatorAccountId as AccountId,
-            -(transfer.amount * multiplier),
-          );
-        } else {
-          transaction.addApprovedTokenTransfer(
-            assetid,
-            transfer.from as string,
-            -(transfer.amount * multiplier),
-          );
-        }
-
-        // Service Fee
-        if (options.serviceFeesToPay[assetid] > 0) {
-          transaction.addTokenTransfer(
-            assetid,
-            serviceFeeToAddr,
-            options.serviceFeesToPay[assetid] * multiplier,
-          );
-          transaction.addTokenTransfer(
-            assetid,
-            client.operatorAccountId as AccountId,
-            -(options.serviceFeesToPay[assetid] * multiplier),
-          );
-        }
-      } else if (transfer.assetType === 'NFT') {
-        const assetid = NftId.fromString(transfer.assetId as string);
-        if (_.isEmpty(transfer.from)) {
-          transaction.addNftTransfer(
-            assetid,
-            client.operatorAccountId as AccountId,
-            transfer.to,
-          );
-        } else {
-          transaction.addApprovedNftTransfer(
-            assetid,
-            transfer.from as string,
-            transfer.to,
-          );
-        }
-      }
-    }
-
-    transaction.freezeWith(client);
-
-    const txResponse = await transaction.execute(client);
-
-    options.onBeforeConfirm?.();
 
     const receipt = await txResponse.getReceipt(client);
 
---
+++ packages/hedera-wallet-snap/packages/snap/src/client/commands/TransferCryptoCommand.ts
@@ -0,0 +1,178 @@
+import { SimpleTransfer, TxReceipt } from '../../types/hedera';
+import {
+  AccountId,
+  Client,
+  Hbar,
+  NftId,
+  TransferTransaction,
+} from '@hashgraph/sdk';
+import { ethers } from 'ethers';
+import _ from 'lodash';
+import { Utils } from '../../utils/Utils';
+import { CryptoUtils } from '../../utils/CryptoUtils';
+
+export class TransferCryptoCommand {
+  readonly #transfers: SimpleTransfer[];
+
+  readonly #memo: string | null;
+
+  readonly #maxFee: number | null;
+
+  readonly #serviceFeesToPay: Record<string, number>;
+
+  readonly #serviceFeeToAddress: string | null;
+
+  readonly #onBeforeConfirm?: () => void;
+
+  constructor(
+    transfers: SimpleTransfer[],
+    memo: string | null,
+    maxFee: number | null,
+    serviceFeesToPay: Record<string, number>,
+    serviceFeeToAddress: string | null,
+    onBeforeConfirm?: () => void,
+  ) {
+    this.#transfers = transfers;
+    this.#memo = memo;
+    this.#maxFee = maxFee;
+    this.#serviceFeesToPay = serviceFeesToPay;
+    this.#serviceFeeToAddress = serviceFeeToAddress;
+    this.#onBeforeConfirm = onBeforeConfirm;
+  }
+
+  public async execute(client: Client): Promise<TxReceipt> {
+    const serviceFeeToAddr: string = this.#serviceFeeToAddress ?? '0.0.98'; // 0.0.98 is Hedera Fee collection account
+
+    const transaction = new TransferTransaction().setTransactionMemo(
+      this.#memo ?? '',
+    );
+
+    if (this.#maxFee) {
+      transaction.setMaxTransactionFee(new Hbar(this.#maxFee.toFixed(8)));
+    }
+
+    for (const transfer of this.#transfers) {
+      if (transfer.assetType === 'HBAR') {
+        if (ethers.isAddress(transfer.to)) {
+          transfer.to = `0.0.${transfer.to.slice(2)}`;
+        }
+
+        transaction.addHbarTransfer(transfer.to, transfer.amount);
+        if (_.isEmpty(transfer.from)) {
+          transaction.addHbarTransfer(
+            client.operatorAccountId as AccountId,
+            -transfer.amount,
+          );
+        } else {
+          transaction.addApprovedHbarTransfer(
+            transfer.from as string,
+            -transfer.amount,
+          );
+        }
+
+        // Service Fee
+        if (this.#serviceFeesToPay[transfer.assetType] > 0) {
+          transaction.addHbarTransfer(
+            serviceFeeToAddr,
+            this.#serviceFeesToPay[transfer.assetType],
+          );
+          transaction.addHbarTransfer(
+            client.operatorAccountId as AccountId,
+            -this.#serviceFeesToPay[transfer.assetType],
+          );
+        }
+      } else if (transfer.assetType === 'TOKEN') {
+        const assetid = transfer.assetId as string;
+        const multiplier = Math.pow(10, transfer.decimals as number);
+
+        transaction.addTokenTransfer(
+          assetid,
+          transfer.to,
+          transfer.amount * multiplier,
+        );
+        if (_.isEmpty(transfer.from)) {
+          transaction.addTokenTransfer(
+            assetid,
+            client.operatorAccountId as AccountId,
+            -(transfer.amount * multiplier),
+          );
+        } else {
+          transaction.addApprovedTokenTransfer(
+            assetid,
+            transfer.from as string,
+            -(transfer.amount * multiplier),
+          );
+        }
+
+        // Service Fee
+        if (this.#serviceFeesToPay[assetid] > 0) {
+          transaction.addTokenTransfer(
+            assetid,
+            serviceFeeToAddr,
+            this.#serviceFeesToPay[assetid] * multiplier,
+          );
+          transaction.addTokenTransfer(
+            assetid,
+            client.operatorAccountId as AccountId,
+            -(this.#serviceFeesToPay[assetid] * multiplier),
+          );
+        }
+      } else if (transfer.assetType === 'NFT') {
+        const assetid = NftId.fromString(transfer.assetId as string);
+        if (_.isEmpty(transfer.from)) {
+          transaction.addNftTransfer(
+            assetid,
+            client.operatorAccountId as AccountId,
+            transfer.to,
+          );
+        } else {
+          transaction.addApprovedNftTransfer(
+            assetid,
+            transfer.from as string,
+            transfer.to,
+          );
+        }
+      }
+    }
+
+    transaction.freezeWith(client);
+
+    const txResponse = await transaction.execute(client);
+
+    this.#onBeforeConfirm?.();
+
+    const receipt = await txResponse.getReceipt(client);
+
+    let newExchangeRate;
+    if (receipt.exchangeRate) {
+      newExchangeRate = {
+        ...receipt.exchangeRate,
+        expirationTime: Utils.timestampToString(
+          receipt.exchangeRate.expirationTime,
+        ),
+      };
+    }
+
+    return {
+      status: receipt.status.toString(),
+      accountId: receipt.accountId ? receipt.accountId.toString() : '',
+      fileId: receipt.fileId ? receipt.fileId : '',
+      contractId: receipt.contractId ? receipt.contractId : '',
+      topicId: receipt.topicId ? receipt.topicId : '',
+      tokenId: receipt.tokenId ? receipt.tokenId : '',
+      scheduleId: receipt.scheduleId ? receipt.scheduleId : '',
+      exchangeRate: newExchangeRate,
+      topicSequenceNumber: receipt.topicSequenceNumber
+        ? String(receipt.topicSequenceNumber)
+        : '',
+      topicRunningHash: CryptoUtils.uint8ArrayToHex(receipt.topicRunningHash),
+      totalSupply: receipt.totalSupply ? String(receipt.totalSupply) : '',
+      scheduledTransactionId: receipt.scheduledTransactionId
+        ? receipt.scheduledTransactionId.toString()
+        : '',
+      serials: JSON.parse(JSON.stringify(receipt.serials)),
+      duplicates: JSON.parse(JSON.stringify(receipt.duplicates)),
+      children: JSON.parse(JSON.stringify(receipt.children)),
+    } as TxReceipt;
+  }
+}
