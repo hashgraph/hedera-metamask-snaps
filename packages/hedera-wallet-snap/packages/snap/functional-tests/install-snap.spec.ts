569b8e25915ed4338c2af8aa070fbd4934d8a788
Ivy Astrix <ivy.astrix@gmail.com>
2024-03-19 23:02:12 -0700

    moved tests to functional-tests/ subfolder
    Signed-off-by: Ivy Astrix <ivy.astrix@gmail.com>

--- packages/hedera-wallet-snap/packages/snap/functional-tests/install-snap.spec.ts
+++ packages/hedera-wallet-snap/packages/snap/functional-tests/install-snap.spec.ts
@@ -1,13 +1,50 @@
+import type { Snap } from '@metamask/snaps-jest';
 import { installSnap } from '@metamask/snaps-jest';
+import { expect } from '@jest/globals';
+import { panel, text } from '@metamask/snaps-sdk';
 
-describe('MySnap', () => {
-    it('should do something', async () => {
-    const { request } = await installSnap();
-    const response = await request({
-      method: 'testingFunctionalTests',
-      params: [],
+describe('Hedera Wallet Snap', () => {
+  it('Should return a valid Snap object', async () => {
+    const snapObj: Snap = await installSnap();
+    expect(snapObj).toHaveProperty('request');
+  });
+
+  it('Should return a confirmation dialog for the Hello RPC method', async () => {
+    const snapObj: Snap = await installSnap();
+    const origin = 'Jest';
+    const response = snapObj.request({
+      method: 'hello',
+      origin,
     });
 
-    expect(response).toBe('hi');
+    const ui = await response.getInterface();
+    expect(ui.type).toBe('confirmation');
+    expect(ui).toRender(
+      panel([
+        text(`Hello, **${origin}**!`),
+        text(
+          "You are seeing this because you interacted with the 'hello' method",
+        ),
+      ]),
+    );
+
+    await ui.ok();
+
+    expect(await response).toRespondWith(true);
+  });
+
+  it('Should throw an error when an invalid method is specified', async () => {
+    const snap = await installSnap();
+    const request = snap.request.bind(snap);
+
+    const response = await request({
+      method: 'foo',
+    });
+
+    expect(response).toRespondWithError({
+      code: -32603,
+      message: 'Method not found.',
+      stack: expect.any(String),
+    });
   });
 });
