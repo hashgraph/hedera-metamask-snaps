2615986454e37837bfdfc24179abf2b31eb4b5f8
Himalayan Dev <kipkap1001@gmail.com>
2024-10-11 16:34:18 -0500

    Updated some dependencies
    Signed-off-by: Himalayan Dev <kipkap1001@gmail.com>

--- packages/hedera-identify-snap/packages/snap/global.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-
-import { MetaMaskInpageProvider } from '@metamask/providers';
-import { SnapsGlobalObject } from '@metamask/snaps-types';
-
-declare global {
-  let ethereum: MetaMaskInpageProvider;
-  let snap: SnapsGlobalObject;
-}
