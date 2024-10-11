e8c63ab0f46665e0d577018a23e77a041ad36917
Himalayan Dev <kipkap1001@gmail.com>
2024-10-11 13:21:05 -0500

    Fixed gatsby issue with example site
    Signed-off-by: Himalayan Dev <kipkap1001@gmail.com>

--- packages/hedera-identify-snap/packages/site/gatsby-config.ts
+++ packages/hedera-identify-snap/packages/site/gatsby-config.ts
@@ -1,3 +1,23 @@
+/*-
+ *
+ * Hedera Wallet Snap
+ *
+ * Copyright (C) 2024 Hedera Hashgraph, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 import { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
@@ -10,7 +30,7 @@ const config: GatsbyConfig = {
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
-        name: 'Identity Snap',
+        name: 'Identify Demo',
         icon: 'src/assets/logo.svg',
         theme_color: '#6F4CFF',
         background_color: '#FFFFFF',
